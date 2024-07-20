95a6d107459dd32956683eb41b8fef4d8fafc089
thapanirajan <thapanirajan789@gmail.com>
2024-07-20 10:45:13 +0545

    add new files

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const port = 3000;
-// const path = require("path");
-
-// app.use(express.urlencoded({ extended: true }));
-// app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname, "views"));
-// app.use(express.static(path.join(__dirname, "public")));
-
-// let posts = [
-//   {
-//     username: "Apnacollege",
-//     content: "I love coding",
-//   },
-//   {
-//     username: "sharadhaKhapra",
-//     content: "hard work is important to achieve success",
-//   },
-//   {
-//     username: "username1",
-//     content: "I got selected for my 1st intership",
-//   },
-// ];
-
-// app.get("/posts", (req, res) => {
-//   res.render("index.ejs", { posts });
-// });
-// app.listen(port, () => {
-//   console.log(`Listening to port ${port}`);
-// });
-
 const { render } = require("ejs");
 const express = require("express");
 const app = express();
